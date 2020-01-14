a93988d759b6340467f3c728e32fe0b0717c102c
amannm <amannm@DEVPCWIN707>
2020-01-14 09:08:37 -0500

    Initial commit

---
+++ GatewayDowntown/GatewayDowntown/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GatewayDowntown.MvcApplication" Language="C#" %>
---
+++ GatewayDowntown/GatewayDowntown/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GatewayDowntown.Startup))]
+namespace GatewayDowntown
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
